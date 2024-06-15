718ff2a2fe1d58ef91b5454e5dc6f5b0f836d889
Lostii <dreamcirno9@foxmail.com>
2024-06-15 12:46:18 +0800

    Luban 环境配置

--- Configs/GameConfig/gen_code_bin_to_project.bat
+++ Configs/GameConfig/gen_code_bin_to_project.bat
@@ -4,10 +4,10 @@ echo %CD%
 set WORKSPACE=../..
 set LUBAN_DLL=%WORKSPACE%\Tools\Luban\Luban.dll
 set CONF_ROOT=.
-set DATA_OUTPATH=%WORKSPACE%/UnityProject/Assets/AssetRaw/Configs/bytes/
-set CODE_OUTPATH=%WORKSPACE%/UnityProject/Assets/GameScripts/HotFix/GameProto/GameConfig/
+set DATA_OUTPATH=%WORKSPACE%/ProjectAlpha/Assets/AssetRaw/Configs/bytes/
+set CODE_OUTPATH=%WORKSPACE%/ProjectAlpha/Assets/GameScripts/HotFix/GameProto/GameConfig/
 
-xcopy /s /e /i /y "%CONF_ROOT%\CustomTemplate\ConfigSystem.cs" "%WORKSPACE%\UnityProject\Assets\GameScripts\HotFix\GameProto\ConfigSystem.cs"
+xcopy /s /e /i /y "%CONF_ROOT%\CustomTemplate\ConfigSystem.cs" "%WORKSPACE%\ProjectAlpha\Assets\GameScripts\HotFix\GameProto\ConfigSystem.cs"
 
 dotnet %LUBAN_DLL% ^
     -t client ^
